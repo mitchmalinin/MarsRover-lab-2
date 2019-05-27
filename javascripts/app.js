ee4718e78e0a1dafcedc841b79515da1f1df868f
Mitch Malinin <mitchmalinin@gmail.com>
2019-05-27 01:04:48 -0400

    First Commit

---
+++ javascripts/app.js
@@ -0,0 +1,22 @@
+// Rover Object Goes Here
+// ======================
+
+// ======================
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
+
+var rover = {
+  key1: ["N", "S","E","W",]
+  
+}
+
+console.log(rover);
